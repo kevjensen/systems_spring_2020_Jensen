efc1cf01be9dc14acc2cdf7e33bd6bca1f4ed07e
Kevin Jensen <kevin.jensen42@du.edu>
2020-04-21 15:08:05 -0600

    Upload New File

---
+++ bash_scripts/countthes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+infile="alice.txt"
+
+#Prints lines if it has the word "the" in it; unfortunately, I think this also includes words like "then"
+while read line;do
+	grep -o 'the' | wc -l
+done < $infile
+
+
