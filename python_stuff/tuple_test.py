5058fa1fd7f9f69b0ff2d3fbcf298e633b34b71e
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:09:25 -0600

    Tuple Test

---
+++ python_stuff/tuple_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+tuplesfordayz = ("strings rule", 8, "WoW > ", 9)
+
+print("First tuple index: {0}".format(tuplesfordayz[0]))
+
+print("Second tuple index: {0}".format(tuplesfordayz[1]))
+
+print("All tuple indices: {0}".format(tuplesfordayz))
+
+#tuplesfordayz[1] = ("hell yea brother")
+
+#print("New second tuple index: {0}".format(tuplesfordayz[1]))
+
+#Received an error which stated tuples do not support assignment. Just another thing that C++ does better.
