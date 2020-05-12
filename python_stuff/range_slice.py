1ef0aa61057f01b199577e776f79b541c7e2eaa9
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:21:54 -0600

    Range Slice

---
+++ python_stuff/range_slice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+three_list = list(range(3, 102, 3))
+
+print(three_list)
+
+#Slice the first 10 elements
+
+print(three_list[0:10])
+
+#Slice the last 10 elements
+
+print(three_list[-10:])
+
+#Size of list
+
+print(len(three_list))
