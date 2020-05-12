8723c28295df51d307d8bd89be28b6e2df02eec0
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:08:10 -0600

    Sys Import1

---
+++ python_stuff/sys_import1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import sys
+
+print("Number of command line arguments: {0}".format(len(sys.argv)))
+
+print("Length of first command line argument: {0}".format(len(sys.argv[1])))
+
+print("Entire argv list: {0}".format(str(sys.argv)))
