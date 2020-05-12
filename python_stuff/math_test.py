884ec98774c069b38dfb3fe1f1870202049c551a
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:23:26 -0600

    Math Test

---
+++ python_stuff/math_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import math
+
+userinput = input("Please enter a number.")
+userinput2 = input("Please enter a second number.")
+
+input1 = int(userinput)
+input2 = int(userinput2)
+
+print("First input divided by second input is: {0}".format(input1 / input2))
+
+print("Remainder of division is: {0}".format(input1 % input2))
