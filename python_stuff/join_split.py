06a8160b2b613ed91f5226d3e3adc170507ec6b2
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:18:29 -0600

    Join Split

---
+++ python_stuff/join_split.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+strvar = "this,is,a,list,5"
+
+#Split the above string into a list using a comma delimiter
+
+newlist = strvar.split(",")
+
+print(newlist)
+
+join_str_list = " ".join(newlist)
+
+print(join_str_list)
+
