6e11b4aded23df5ab48f08de15f71e46e83d3850
Kevin Jensen <kevin.jensen42@du.edu>
2020-05-12 11:08:30 -0600

    Sys Import2

---
+++ python_stuff/sys_import2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import sys as stuffnatemakesusdoforfun
+
+print("Number of command line arguments: {0}".format(len(stuffnatemakesusdoforfun.argv)))
+
+print("Length of first command line argument: {0}".format(len(stuffnatemakesusdoforfun.argv[1])))
+
+print("Entire argv list: {0}".format(str(stuffnatemakesusdoforfun.argv)))
